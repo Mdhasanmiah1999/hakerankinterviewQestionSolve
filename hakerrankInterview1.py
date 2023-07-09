e948e2d216cb74d97d5130a1132fc0d74d6e3f7a
Md Hasan Miah <110524589+Mdhasanmiah1999@users.noreply.github.com>
2023-07-09 08:29:56 +0600

    Add files via upload
    1.2  Sock Merchant
    Problem
    John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
    
    For example, there are n=7 socks with colors ar=[1,2,1,2,1,3,2]. There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
    
    
    Function Description
    
    Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
    
    sockMerchant has the following parameter(s):
    
    n: the number of socks in the pile
    ar: the colors of each sock
    
    Input Format
    The first line contains an integer n, the number of socks represented in ar.
    
    The second line contains n space-separated integers describing the colors ar[i] of the socks in the pile.
    
    
    Constraints
    1 <= n <= 100
    1 <= ar[i] <= 100 where 0 <= i <= n
    
    Output Format
    Return the total number of matching pairs of socks that John can sell.
    
    
    Sample Input
    
    9
    10 20 20 10 10 30 50 10 20

---
+++ hakerrankInterview1.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    return sum([v//2 for k,v in {i:ar.count(i) for i in ar}.items()])
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
